902045549667231dfbaca67605740c62108ed94b
Cody A. Ray <cray@signal.co>
2014-11-19 19:51:23 -0600

    [adhoc:dalton] Refactor/simplify dalton analysis webpage

--- analysis.py
+++ analysis.py
@@ -6,7 +6,6 @@ Runs an analysis on your EC2 Security Group instance assignments.
 from boto.ec2 import connect_to_region
 import re
 import yaml
-from time import time
 from cachetools import ttl_cache
 from flask import Flask, render_template
 
@@ -17,9 +16,9 @@ UNDERSCORE = re.compile("_")
 
 @ttl_cache(ttl=5*60)
 def get_all_reservations(ec2):
-    return ec2.get_all_reservations()
+  return ec2.get_all_reservations()
 
-def fetch_records(env):
+def fetch_hosts_and_groups(env):
   credentials = yaml.load(open('config/aws.yaml', 'r').read())[env]
 
   records = []
@@ -27,7 +26,7 @@ def fetch_records(env):
     ec2 = connect_to_region(region, **credentials)
     for reservation in get_all_reservations(ec2):
       if len(reservation.instances) > 1:
-        print "More than one instance in this reservation: %s" % reservation.id
+        print "Skipping reservation with more than one instance: %s" % reservation.id
         continue
 
       instance = reservation.instances[0]
@@ -37,29 +36,37 @@ def fetch_records(env):
       m = DIGIT.search(name)
       role = name[0:m.start()] if m else "unknown"
 
-      if "default" not in groups:
-        print "%s missing default group" % name
-      groups = [group for group in groups if group != "default"]
-
-      has_correct_roles = len(groups) > 0
-      for group in groups:
-        if group.startswith("cassandra_"):
-          m = UNDERSCORE.search(group)
-          g = group[m.start()+1:len(group)] if m else group
-        else:
-          g = "graph" if group == "graphite" else group
-        has_correct_roles = has_correct_roles and role == g
-      records.append([region, role, name, groups, has_correct_roles])
+      records.append([region, role, name, groups])
   return records
+
+def analyze_security_group_assignments(records):
+  analyzed = []
+  for region, role, name, groups in records:
+    if "default" not in groups:
+      print "%s missing default group" % name
+    groups = [group for group in groups if group != "default"]
+    has_correct_groups = check_has_correct_security_groups(role, groups)
+    analyzed.append([region, role, name, groups, has_correct_groups])
+  return analyzed
+
+def check_has_correct_security_groups(role, security_groups):
+  has_correct_groups = len(security_groups) > 0
+  for group in security_groups:
+    if group.startswith("cassandra_"):
+      m = UNDERSCORE.search(group)
+      g = group[m.start()+1:len(group)] if m else group
+    else:
+      g = "graph" if group == "graphite" else group
+    has_correct_groups = has_correct_groups and role == g
+  return has_correct_groups
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/<env>')
 def security_group_assignments(env='prod'):
-  records = fetch_records(env)
+  records = analyze_security_group_assignments(fetch_hosts_and_groups(env))
   return render_template('assignments.html', records=records)
 
 if __name__ == '__main__':
   app.run(debug=True)
-
